625b35cb5ebd7558f6756be33f72daafb141ffa0
imtiyazmygit <imtiyaz@github.com>
2026-06-29 21:19:11 +0530

    Update Hostinger DB connection settings

--- config/database.php
+++ config/database.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
 
-$host = '127.0.0.1:3306';
+$host = 'localhost';
+$port = '3306';
 $dbname = 'u767322683_idiscourse';
 $username = 'u767322683_admin';
 $password = 'Hurify@1234';
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
